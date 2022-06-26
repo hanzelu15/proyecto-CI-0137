961db13bd65da6762411d4b363fb5ac8763fb6c0
Hansel Calderon <Hanzel_u15@hotmail.com>
2022-06-25 22:34:40 -0600

    backend archivos faltantes

--- Backend/routes/phases.js
+++ Backend/routes/phases.js
@@ -3,7 +3,6 @@ const { Router } = require("express");
 const {
  getPhases,
   createPhase,
-  readPhase,
   updatePhase,
   deletePhase,
 } = require("../controllers/phases");
@@ -11,7 +10,6 @@ const {
 const router = Router();
 
 router.get("/", getPhases);
-router.get("/:id", readPhase);
 router.post("/", createPhase);
 router.put("/update/:id", updatePhase);
 router.delete("/", deletePhase);

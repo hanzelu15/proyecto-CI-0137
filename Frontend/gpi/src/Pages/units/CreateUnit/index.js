97749b0bc42fa1212d18abbaa419b03361ecf322
Hansel Calderon <Hanzel_u15@hotmail.com>
2022-07-10 10:11:17 -0600

    azgregados mas campos a crear unidad

--- Frontend/gpi/src/Pages/units/CreateUnit/index.js
+++ Frontend/gpi/src/Pages/units/CreateUnit/index.js
@@ -4,11 +4,10 @@ import { createUnit } from "../../../Services/UnitService";
 import Swal from "sweetalert2";
 import { useNavigate, useLocation } from "react-router-dom";
 
-
 export const CreateUnit = () => {
   let navigate = useNavigate();
   const location = useLocation();
-  const phase= location.state;
+  const phase = location.state;
   const {
     register,
     handleSubmit,
@@ -17,20 +16,20 @@ export const CreateUnit = () => {
 
   const onSubmit = async (data) => {
     const newUnit = {
-      ...data, 
+      ...data,
       phase: phase._id,
     };
-    const response = await createUnit (newUnit);
-    if (response.ok){
+    const response = await createUnit(newUnit);
+    if (response.ok) {
       Swal.fire("Exito!", "Se ha agregado la unidad!", "success");
       return navigate(-1);
     }
-    return Swal.fire("Ups!", "No se pudo crear la unidad!", "error")
+    return Swal.fire("Ups!", "No se pudo crear la unidad!", "error");
   };
   return (
     <>
       <div className="flex flex-col justify-items-center items-center responsive-width-component">
-        <h4 className="mb-14 text-3xl md:text-4xl" style={{ padding: 35}}>
+        <h4 className="mb-14 text-3xl md:text-4xl" style={{ padding: 35 }}>
           Agregar Unidad para {phase.name}
         </h4>
         <form onSubmit={handleSubmit(onSubmit)} className="w-full">
@@ -40,11 +39,13 @@ export const CreateUnit = () => {
               name="name"
               className="input-text-template peer"
               placeholder=" "
-              {...register("name", {              
+              {...register("name", {
                 maxLength: 40,
-              })}           
+              })}
             />
-            <label className="input-text-label-template">Nombre del cliente (opcional)</label>
+            <label className="input-text-label-template">
+              Nombre del cliente (opcional)
+            </label>
           </div>
           <div className="relative z-0 w-full mb-14 group">
             <input
@@ -58,16 +59,41 @@ export const CreateUnit = () => {
               })}
               required
             />
-            <label className="input-text-label-template">Número de unidad</label>
+            <label className="input-text-label-template">
+              Número de unidad
+            </label>
           </div>
-        
-
+          <div className="relative z-0 w-full mb-14 group">
+            <p className="text-xl text-gray-500">Casa terminada</p>
+            <select
+              className="bg-transparent border border-gray-300 text-gray-500  text-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              {...register("role", {})}
+            >
+              <option defaultValue={false} value={false}>
+                {" "}
+               No
+              </option>
+              <option value={true}>Sí</option>
+            </select>
+          </div>
+          <div className="relative z-0 w-full mb-14 group">
+            <p className="text-xl text-gray-500">Casa entregada</p>
+            <select
+              className="bg-transparent border border-gray-300 text-gray-500  text-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              {...register("role", {})}
+            >
+              <option defaultValue={false} value={false}>
+                {" "}
+               No
+              </option>
+              <option value={true}>Sí</option>
+            </select>
+          </div>
           <div className="flex justify-end  items-center mb-14">
             <input type="submit" value="Agregar Unidad" className="btn-green" />
           </div>
         </form>
       </div>
     </>
-    
-  )
-}
+  );
+};
